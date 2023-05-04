69e9e85e85a01e853347fe266f1ff9174f250f40
Claire <claire.bk.seo@gmail.com>
2023-05-04 11:09:16 +1000

    product detail

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,5 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+import Button from "../components/ui/Button";
 
 export default function ProductDetail() {
-  return <div>Product Detail</div>;
+  const {
+    state: {
+      product: { id, image, title, description, category, price, options },
+    },
+  } = useLocation();
+  const [selected, setSelected] = useState(options && options[0]);
+  const handleSelect = (e) => setSelected(e.target.value);
+  const handleClick = (e) => {};
+
+  return (
+    <>
+      <p className="mx-12 mt-4 text-gray-700">{category}</p>
+      <section className="flex flex-col md:flex-row p-4">
+        <div className="w-full px-4 basis-7/12">
+          <img src={image} alt={title} />
+        </div>
+        <div className="w-full basis-5/12 flex flex-col p-4">
+          <h2 className="text-3xl font-bold py-2">{title}</h2>
+          <p className="text-2xl font-bold py-2 border-b border-dray-400">
+            ${price}
+          </p>
+          <p className="py-4 text-lg">{description}</p>
+          <div className="flex items-center">
+            <lable htmlFor="select" className="text-brand font-bold">
+              option :
+            </lable>
+            <select
+              id="select"
+              className="p-2 m-4 flex-1 border-2 border-dashed border-brand outline-none"
+              onChange={handleSelect}
+              value={selected}
+            >
+              {options &&
+                options.map((option, index) => (
+                  <option key={index}>{option}</option>
+                ))}
+            </select>
+          </div>
+          <Button text="Add to cart" onClick={handleClick} />
+        </div>
+      </section>
+    </>
+  );
 }
